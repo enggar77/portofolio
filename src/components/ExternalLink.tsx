dd31b17bb922d222103c90b10ca874c3906a9878
enggar <ejihandoko@gmail.com>
2025-02-23 16:54:07 +0700

    update link

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ForwardIcon from '@/data/forwardIcon';
-import NextLink from 'next/link';
+import Link from 'next/link';
 
 type Props = {
 	children: React.ReactNode;
@@ -11,13 +11,13 @@ export default function ExternalLink({ children, link }: Props) {
 	return (
 		<div className="w-fit group">
 			<div className="flex items-center gap-2 text-sm">
-				<NextLink
+				<Link
 					href={link}
 					target="_blank"
 					className="group-hover:text-base-content/50 transition-colors"
 				>
 					{children}
-				</NextLink>
+				</Link>
 				<ForwardIcon
 					width={11}
 					className="fill-current transition-all group-hover:opacity-50"
